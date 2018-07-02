7a0044be2ff544ebb8972ca8aec859a6f2f87abf
Scott <scrosehart@gmail.com>
2018-07-02 12:17:30 -0700

    styling iphone x

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -11,17 +11,17 @@ class Navbar extends Component {
     if (this.props.user) {
       return (
         <div className='flexbox-nav'>
-          <li data-text='Home'><Link to='/'>Home</Link></li>
-          <li data-text='Profile'><Link to={`/profile/${this.props.user._id}`}>Profile</Link></li>
-          <li data-text='Logout'><Link to='/' onClick={() => this.props.logout()}>Logout</Link></li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to={`/profile/${this.props.user._id}`}>Profile</Link></li>
+          <li><Link to='/' onClick={() => this.props.logout()}>Logout</Link></li>
         </div>
       );
     } else {
       return (
         <div className='flexbox-nav'>
-          <li data-text='Home'><Link to='/'>Home</Link></li>
-          <li data-text='Signup'><Link to='/signup'>Signup</Link></li>
-          <li data-text='Login'><Link to='/login'>Login</Link></li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/signup'>Signup</Link></li>
+          <li><Link to='/login'>Login</Link></li>
         </div>
       );
     }
